bf02090ea0206fccbcdd300d937d7ca9f5b1b1a5
Jacob Zimmerman <jczimm@jczimm.com>
2020-04-18 17:07:08 -0400

    Create About page placeholder

---
+++ jsx/App/AboutLanding.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import id from 'shortid';
+// import { Link } from 'react-router-dom';
+// import ReactDOMServer from 'react-dom/server';
+
+export class AboutLanding extends React.Component {
+    render() {
+        return (
+           <main className="content-wrapper">
+               <header>
+                   <h1>About</h1>
+                </header>
+               <article>
+                   <p>Lorem ipsum dolor sit amet</p>
+                </article>
+           </main>
+        );
+    }
+}
--- jsx/App/App.jsx
+++ jsx/App/App.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { StoryIndex } from './StoryIndex.jsx';
 import { Stories } from './Stories/Stories.jsx';
+import { AboutLanding } from './AboutLanding.jsx';
+// import { ResourcesLanding } from './ResourcesLanding.jsx';
+// import { ResourcesViewMaterials } from './ResourcesViewMaterials.jsx';
 
-export function App({ data }) {
+export function App({ storiesData, resourcesIndex }) {
     return (
         <div>
-            <Route exact path="/index" render={props => <StoryIndex index={data.index} />} />
-            <Route path="/story" render={props => <Stories stories={data.stories} />} />
+            <Route exact path="/index" render={props => <StoryIndex index={storiesData.index} />} />
+            <Route path="/story" render={props => <Stories stories={storiesData.stories} />} />
+            <Route path="/about" render={props => <AboutLanding />} />
+            {/* <Route path="/resources" render={props => <ResourcesLanding curatedResources={resourcesIndex.filter(r => r.isCurated)} />} />
+            <Route path="/resources-view" render={props => <ResourcesViewMaterials resources={resourcesIndex} />} /> */}
         </div>
     );
 }
